5a53ccfdf023186d413651fbac60bf43d533a8d2
Aufa Billah <aufa.jazama@tokopedia.com>
2022-09-03 15:27:19 +0700

    feat: add audio

---
+++ satria-nada/js/index.js
@@ -0,0 +1,13 @@
+function handleAudio() {
+  // play audio
+  const audio = document.getElementById("audio");
+  const btnAudio = document.getElementById("audio-button");
+
+  btnAudio.addEventListener("click", () => {
+    audio.paused ? audio.play() : audio.pause();
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  handleAudio();
+});
