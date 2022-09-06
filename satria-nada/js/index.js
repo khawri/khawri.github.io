9aab0b9fc0a447858bbca25e2073416cda2dfab4
Aufa Billah <aufa.jazama@tokopedia.com>
2022-09-06 20:42:28 +0700

    feat: create modal opening

--- satria-nada/js/index.js
+++ satria-nada/js/index.js
@@ -8,7 +8,24 @@ function handleAudio() {
   });
 }
 
+function playAudio() {
+  const audio = document.getElementById("audio");
+  audio.play();
+}
+
+function openModal() {
+  const modalOpening = new bootstrap.Modal(
+    document.getElementById("modal-opening")
+  );
+  const btnModalClose = document.querySelector(".modal-close");
+
+  modalOpening.show();
+
+  btnModalClose.addEventListener("click", playAudio);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   AOS.init();
   handleAudio();
+  openModal();
 });
